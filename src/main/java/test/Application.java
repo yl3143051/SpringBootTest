21ed87e53d26005888ec7d0162f273e75d070950
taieo <13906569268@163.com>
2017-10-17 20:53:13 +0800

    Initial commit

---
+++ src/main/java/test/Application.java
@@ -0,0 +1,36 @@
+package test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+@RestController
+@EnableAutoConfiguration
+//在主函数中实现EmbeddedServletContainerCustomizer接口来重写方法修改内置Tomcat的端口号
+public class Application implements EmbeddedServletContainerCustomizer{
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
+
+    @RequestMapping("/now")
+    String hehe() {
+        return "现在时间：" + (new Date()).toLocaleString();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public void customize(ConfigurableEmbeddedServletContainer configurableEmbeddedServletContainer) {
+        configurableEmbeddedServletContainer.setPort(8082);
+    }
+}
